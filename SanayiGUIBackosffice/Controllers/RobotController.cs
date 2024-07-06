c4d6bf8cc7ce765e6d9914485a3d868ea10e0616
osman <oceylankou@gmail.com>
2024-07-06 18:01:42 +0300

    draw obstacle developments

--- SanayiGUIBackosffice/Controllers/RobotController.cs
+++ SanayiGUIBackosffice/Controllers/RobotController.cs
@@ -13,6 +13,7 @@ namespace SanayiGUIBackosffice.Controllers
     public class RobotController : ControllerBase
     {
         IWebSocketManager _webSocketManager;
+        public static List<Point> Points { get; set; } = new List<Point>();
 
         public RobotController(IWebSocketManager webSocketManager)
         {
@@ -68,7 +69,36 @@ namespace SanayiGUIBackosffice.Controllers
         [HttpPost]
         public async Task DrawObstacle(DrawObstacleRequestMessage requestMessage)
         {
+            //for (var i = 5000; i <= 10000; i+=100)
+            //{
+            //    for (int ii = 0; ii < 100; ii++)
+            //    {
+            //        Points.Add(new Point
+            //        {
+            //            X = i+ii,
+            //            Y = i,
+            //        });
+            //    }
 
+            //    requestMessage.Points = Points;
+            //    var uiMessage = new UiDataRequestMessage<DrawObstacleRequestMessage>
+            //    {
+            //        ClassType = nameof(DrawObstacleRequestMessage),
+            //        Data = requestMessage
+            //    };
+            //    var uiMessageJson = JsonSerializer.Serialize(uiMessage);
+            //    await this._webSocketManager.BroadcastMessageAsync(uiMessageJson);
+            //    Thread.Sleep(5);
+            //}
+            Points.AddRange(requestMessage.Points);
+            requestMessage.Points = Points;
+            var uiMessage = new UiDataRequestMessage<DrawObstacleRequestMessage>
+            {
+                ClassType = nameof(DrawObstacleRequestMessage),
+                Data = requestMessage
+            };
+            var uiMessageJson = JsonSerializer.Serialize(uiMessage);
+            await this._webSocketManager.BroadcastMessageAsync(uiMessageJson);
         }
     }
 }
--- SanayiGUIBackosffice/Messages/DrawObstacleRequestMessage.cs
+++ SanayiGUIBackosffice/Messages/DrawObstacleRequestMessage.cs
@@ -4,6 +4,7 @@ namespace SanayiGUIBackosffice.Messages
 {
     public class DrawObstacleRequestMessage
     {
+        public List<Point> Points { get; set; }
         public Point? StartPoint { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
